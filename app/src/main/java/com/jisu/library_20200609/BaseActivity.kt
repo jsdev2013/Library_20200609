2df968cd6961613b6a97e304a614c16b316ffa66
JISU LEE <jsnote2013@gmail.com>
2020-06-09 19:34:43 +0900

    기본세팅

---
+++ app/src/main/java/com/jisu/library_20200609/BaseActivity.kt
@@ -0,0 +1,12 @@
+package com.jisu.library_20200609
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+
+}
