dc224b0e069c3e75a34fafc5664078ee716c7ea9
JISU LEE <jsnote2013@gmail.com>
2020-06-09 20:21:21 +0900

    확대축소가 가능한 PhoteView 를 사용

---
+++ app/src/main/java/com/jisu/library_20200609/LargePhotoActivity.kt
@@ -0,0 +1,23 @@
+package com.jisu.library_20200609
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LargePhotoActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_large_photo)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+
+}
--- app/src/main/java/com/jisu/library_20200609/MainActivity.kt
+++ app/src/main/java/com/jisu/library_20200609/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.jisu.library_20200609
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,10 @@ class MainActivity : BaseActivity() {
     }
     override fun setupEvents() {
 
+        profileImg.setOnClickListener {
+            val myIntent = Intent(mContext, LargePhotoActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 
     override fun setValues() {
