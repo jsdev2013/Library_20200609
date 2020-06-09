914085613fdefa1dcdbf496c68b0f6336507bcec
JISU LEE <jsnote2013@gmail.com>
2020-06-09 19:31:52 +0900

    Initial commit

---
+++ app/src/main/java/com/jisu/library_20200609/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jisu.library_20200609
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
