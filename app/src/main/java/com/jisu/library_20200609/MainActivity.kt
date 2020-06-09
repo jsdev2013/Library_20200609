af44f6d5b952e269b6cdfc81a609b3dd90416b04
JISU LEE <jsnote2013@gmail.com>
2020-06-09 21:11:08 +0900

    Glide라이브러리로 웹상의 이미지 불러오기 - Internet 권한 사용 명시 - Http:// 주소의 이미지는 못 불러온다.

--- app/src/main/java/com/jisu/library_20200609/MainActivity.kt
+++ app/src/main/java/com/jisu/library_20200609/MainActivity.kt
@@ -2,6 +2,7 @@ package com.jisu.library_20200609
 
 import android.content.Intent
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
@@ -20,6 +21,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-
+        Glide.with(mContext).load("https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png").into(profileImg)
     }
 }
